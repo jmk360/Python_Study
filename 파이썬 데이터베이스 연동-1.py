fdb625c58986ece553b39a0259545847273e9153
jmk360 <jmk360@daum.net>
2023-01-17 02:04:33 +0900

    수정

---
+++ 파이썬 데이터베이스 연동-1.py
@@ -0,0 +1,51 @@
+# Section12-1
+# 파이썬 데이터베이스 연동(SQLite)
+# 테이블 생성 및 삽입
+
+import sqlite3
+import datetime
+
+# 삽입 날짜 생성
+now = datetime.datetime.now()
+print('now : ', now)
+
+nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+print('nowDatetime : ', nowDatetime)
+
+# sqlite3
+print('sqlite3.version : ', sqlite3.version) # sqlite 버전
+print('sqlite3.sqlite_version : ', sqlite3.sqlite_version) # sqlite 엔진 버전
+
+# DB 생성 & Auto Commit(Rollback)
+conn = sqlite3.connect('C:/dave/fastcampus/Coding_Algorithem/python_basic/resource/database.db', isolation_level=None)
+
+# Cursor
+c = conn.cursor()
+print('Cursor type : ', type(c))
+
+# 테이블 생성(Data Type : TEXT, NUMERIC, INTEGER, REAL, BLOB)
+c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text)")
+
+# 데이터 삽입
+# c.execute("INSERT INTO users VALUES(1, 'kim', 'kim@naver.com', '010-0000-0000', 'kim.com', ?)", (nowDatetime,)) # ?는 튜플 형태로 매칭시켜야 한다.
+# c.execute("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", (2, 'Park', 'Park@daum.net', '010-1111-1111', 'Park.com', nowDatetime))
+
+# Many 삽입(튜플, 리스트)
+userList = (
+    (3, 'Lee', 'Lee@naver.com', '010-2222-2222', 'Lee.com', nowDatetime),
+    (4, 'Cho', 'Cho@daum.net', '010-3333-3333', 'Cho.com', nowDatetime),
+    (5, 'Yoo', 'Yoo@google.com', '010-4444-4444', 'Yoo.net', nowDatetime)
+)
+
+# c.executemany("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)", userList)
+
+# 테이블 데이터 삭제
+# conn.execute("DELETE FROM users")
+# print("users db deleted : ", conn.execute("DELETE FROM users").rowcount)
+
+# 커밋 : isolation_level = None 일 경우 자동 반영(오토 커밋) 
+# -> 만약에 이 옵션을 사용하지 않았다면, conn.commit() 함수를 직접 실행해 주어야 반영이된다.
+# -> conn.rollback() 함수를 통해서 롤백 : 반영이 된것을 되돌린다.
+
+# 접속 해제
+conn.close()
---
+++ 파이썬 외부 파일 처리.py
@@ -0,0 +1,83 @@
+# Section11
+# 파이썬 외부 파일 처리
+# 파이썬 Excel, CSV 파일 읽기 및 쓰기
+
+# CSV : MIME - text/csv
+
+# 예제1
+import csv
+
+with open('./resource/sample1.csv', 'r') as f:
+    reader = csv.reader(f) # delimiter의 기본값은 콤마이다.
+    # next(reader) # Header 스킵 -> 보통은 Header를 스킵하는 경우가 많다.
+    # 확인
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
+    print()
+
+    for c in reader:
+        print(c)
+
+# 예제2
+with open('./resource/sample2.csv', 'r') as f:
+    reader = csv.reader(f, delimiter='|') # delimiter로 구분자를 지정해 줄 수 있다. 기본은 콤마이다.
+    # next(reader) # Header 스킵
+    # 확인
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
+    print()
+
+    for c in reader:
+        print(c)
+
+# 예제3 (Dict변환)
+with open('./resource/sample1.csv', 'r') as f:
+    reader = csv.DictReader(f)
+
+    for c in reader:
+        for k, v in c.items():
+            print(k, v)
+        print('-'*10)
+
+# 예제4
+w = [[1,2,3], [4,5,6], [7,8,9], [10,11,12], [13,14,15], [16,17,18]]
+
+with open('./resource/sample3.csv', 'w', newline='') as f:
+    wt = csv.writer(f)
+
+    for v in w:
+        wt.writerow(v)
+
+# 예제5
+with open('./resource/sample4.csv', 'w', newline='') as f:
+    wt = csv.writer(f)
+    wt.writerows(w)
+
+# XSL, XLSX
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_excel.html
+# openpyxl, xlsxwriter, xlrd, xlwt, xlutil
+# pandas 를 주로 사용(openpyxl, xlrd)
+# pip install xlrd
+# pip install openpyxl
+# pip install pandas
+
+import pandas as pd
+
+# 옵션 : sheetname = '시트명' 또는 숫자, header=숫자, skiprow=숫자
+xlsx = pd.read_excel('./resource/sample.xlsx')
+
+# 상위 데이터 확인
+print(xlsx.head())
+print()
+
+# 데이터 확인
+print(xlsx.tail())
+
+# 데이터 확인
+print(xlsx.shape) # 행, 열
+
+# 엑셀 or CSV 다시 쓰기
+xlsx.to_excel('./resource/result.xlsx', index=False)
+xlsx.to_csv('./resource/result.csv', index=False)
