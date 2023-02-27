ff9ad711e1c8ec3fb28064344b48403450090196
jmk360 <jmk360@daum.net>
2023-02-27 21:21:47 +0900

    수정

--- 파이썬 소개 및 작업 환경 설정.py
+++ 파이썬 소개 및 작업 환경 설정.py
@@ -3,3 +3,8 @@
 
 # 기본 출력
 print("Hello Python!")
+
+# 블로그 정리 할 것 : 
+# task runner 설정하고 ctrl + shift + b 로 코드를 실행시켰을 경우 input함수가 안되는 경우 settings.json 파일 수정하거나,
+# vscode 설정에서 activate 키워드로 검색해서 Python > Terminal: Activate Environment 옵션 해제 하면 됨
+# 관련해서 내용정리하기
